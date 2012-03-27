b4465dbd11b285f16680c9dd9dcaaac61d6c84f6
K Jonathan Harker <kjharke@pdx.edu>
2012-03-27 12:36:00 -0700

    Make sure that the directories exist.

--- link.sh
+++ link.sh
@@ -5,6 +5,11 @@ if [ "$1z" != "yesz" ]
     then exit
 fi
 
+paths="`find . -path ./.git -prune -o -type d -print | grep -v $0`"
+for i in $paths ; do
+    mkdir -p ../$i
+done
+
 files="`find . -path ./.git -prune -o -type f -links 1 -print | grep -v $0`"
 for i in $files ; do
     ln -f $i ../$i

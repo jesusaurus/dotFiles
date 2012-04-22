76eb41f4b974dcfab11bf66e5dace102e7a7d8f1
K Jonathan Harker <jesusaur@cat.pdx.edu>
2012-04-22 13:33:36 -0700

    Merge link.sh from origin/arc

--- link.sh
+++ link.sh
@@ -1,15 +1,17 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
 #did you read the source?
 if [ "$1z" != "yesz" ]
     then exit
 fi
 
+paths="`find . -path ./.git -prune -o -type d -print | grep -v $0`"
+for i in $paths ; do
+    mkdir -p ../$i
+done
+
 files="`find . -path ./.git -prune -o -type f -links 1 -print | grep -v $0`"
 for i in $files ; do
-    if [ ! -d "`dirname $i`" ]; then
-	mkdir -p "`dirname $i`"
-    fi
     ln -f $i ../$i
 done
 

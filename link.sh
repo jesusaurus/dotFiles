93e3b5a91186e27efb29bb6eb802b8c9cd71e73e
K Jonathan Harker <jesusaur@cat.pdx.edu>
2012-05-23 16:18:15 -0700

    Migrate!

--- link.sh
+++ link.sh
@@ -1,17 +1,21 @@
 #!/usr/local/bin/bash
 
-#did you read the source?
-if [ "$1z" != "yesz" ]
-    then exit
-fi
-
 paths="`find . -name .git -prune -o -type d -print | grep -v $0`"
 for i in $paths ; do
     mkdir -p ../$i
 done
 
-files="`find . -name .git -prune -o -type f -links 1 -print | grep -v $0`"
-for i in $files ; do
-    ln -f $i ../$i
-done
+files="`find . -name .git -prune -o -name .gitmodules -prune -o -type f -links 1 -print | grep -v $0`"
+
+if [ "$1z" != "yesz" ]
+then
+	for i in $files ; do
+		diff -u $i ../$i 2>/dev/null 
+	done | less
+else
+	for i in $files ; do
+		ln -f $i ../$i
+	done
+fi
+
 

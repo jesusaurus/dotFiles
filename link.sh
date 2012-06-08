842043491d1e25c010ebb338f8fb3b190bbf4a68
K Jonathan Harker <kjharke@pdx.edu>
2012-06-07 17:23:07 -0700

    A better link.sh

--- link.sh
+++ link.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-#did you read the source?
+paths="`find . -name .git -prune -o -type d -print | grep -v $0`"
+for i in $paths ; do
+    mkdir -p ../$i
+done
+
+files="`find . -name .git -prune -o -name .gitmodules -prune -o -type f -links 1 -print | grep -v $0`"
+
 if [ "$1z" != "yesz" ]
-    then exit
+then
+        #display changes that would occur
+	for i in $files ; do
+		diff -u ../$i $i 2>/dev/null 
+	done | less
+else
+        #irreparable damage may occur
+	for i in $files ; do
+		ln -f $i ../$i
+	done
 fi
 
-files="`find . -path ./.git -prune -o -type f -links 1 -print | grep -v $0`"
-for i in $files ; do
-    ln -f $i ../$i
-done
 

b8013a4b7c6b855de0d3b8e68e826e1164c18ead
K Jonathan Harker <k.jonathan.harker@hp.com>
2013-02-19 11:39:08 -0800

    Add 'short' option to link.sh

--- link.sh
+++ link.sh
@@ -9,15 +9,18 @@ files="`find . -name .git -prune -o -name .gitmodules -prune -o -type f -links 1
 
 if [ "$1z" != "doitz" ]
 then
-        #display changes that would occur
-	for i in $files ; do
-		diff -u ../$i $i 2>/dev/null
-	done | less
+    #display changes that would occur
+    for i in $files ; do
+        if [ "$1" != "short" ]
+        then
+            diff -u ../$i $i 2>/dev/null
+        else
+            diff -u ../$i $i 2>/dev/null | grep '^\(+++\|---\) '
+        fi
+    done | less
 else
-        #irreparable damage may occur
-	for i in $files ; do
-		ln -f $i ../$i
-	done
+    #irreparable damage may occur
+    for i in $files ; do
+        ln -f $i ../$i
+    done
 fi
-
-
